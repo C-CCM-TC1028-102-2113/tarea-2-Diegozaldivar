0f7f660a9d897f6c9e5077f3cf0af4b6f88c982e
Diegozaldivar <89473869+Diegozaldivar@users.noreply.github.com>
2021-08-25 22:14:41 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,8 +1,37 @@
 
+# Este programa determina si a una persona se  le otorga una licencia de conducir o no
+
+
 def main():
-    #Escribe tu código debajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    if edad >= 18:
+        print(' Primera verifivación pasada con éxito ')
+    elif edad <0:
+        print(' Respuesta incorrecta ')
+
+    else:
+        print(' Licencia denegada ')
+    identificacion = str(input( ' Tienes identificaciónoficial? (s/n) ')
+    if identificacion == "si":
+                   print('Licencia autorizada')
+    elif identificacion == "no":
+                        print('Licencia rechazada')
+        
+        
 
 
+                         
+
+    
+
+    
+    
+
+
+       
 if __name__ == '__main__':
     main()
+                         
+    
+ 
+
