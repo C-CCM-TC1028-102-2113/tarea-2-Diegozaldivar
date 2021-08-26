c917b2a41171d2dbf60d9e83598ccc7bded063c2
Diegozaldivar <89473869+Diegozaldivar@users.noreply.github.com>
2021-08-25 22:13:37 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,27 @@
+# Este programa ayuda a determina el IMC
+
+
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-if __name__=='__main__':
+    # Escribe el código adecuado para completar el programa
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+    IMC = peso / altura**2
+    if IMC < 20:
+        print(' PESO BAJO ')
+    if IMC <= 20 and IMC < 25:
+        print(' PESO NORMAL ')
+    if IMC <=25 and  IMC < 30:
+        print(' SOBREPESO ')
+    if IMC <= 30 and IMC < 40:
+        print(' OBESIDAD ')
+    if IMC >= 40:
+        print(' OBESIDAD MORBIDA ')
+    
+
+
+
+
+
+
+if __name__ == '__main__':
     main()
